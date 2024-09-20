0b7a73154fab3866180ae33504080110574eb5b1
MechaBRO <whiteinkpotato@gmail.com>
2024-09-20 18:37:14 -0300

    Update HUD.gd

--- Other/HUD.gd
+++ Other/HUD.gd
@@ -4,7 +4,8 @@ extends CanvasLayer
 @onready var CurrentWeaponLabel = $VBoxContainer/HBoxContainer2/CurrentWeapon
 
 func _on_weapons_manager_update_ammo(Ammo):
-	CurrentAmmoLabel.set_text(str(Ammo[0])+" / "+ str(Ammo[1]))
+	#CurrentAmmoLabel.set_text(str(Ammo[0])+" / "+ str(Ammo[1]))
+	pass
 
 func _on_weapons_manager_weapon_changed(Weapon_Name):
 	pass
